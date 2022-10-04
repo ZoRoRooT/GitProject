a6cdcbfd87a1a6d80d00247d49c872922b82a7f7
ZoRoRooT <sudarshan_n@hotmail.com>
2022-10-04 18:33:44 +0530

    react

--- React-bootstrap-TextUtills/textutills/src/App.js
+++ React-bootstrap-TextUtills/textutills/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Alert from "./Components/Alert";
+import Navbar from "./Components/Navbar";
+import Txtform from "./Components/Txtform";
 
 function App() {
+  const [mode,setMode]=useState("light");
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+  }
+  function toggleMode(){  
+    if(mode==="light"){
+      setMode("dark")
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode hass been enabled","success")
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode hass been enabled","success")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtills" about="About Us" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert}/>
+      <Txtform heading="Enter Text"showAlert={showAlert} mode={mode} />
+    </>
   );
 }
 
