35f94465ee6e11ff69aca8c8d81badd12f16032a
ZoRoRooT <sudarshan_n@hotmail.com>
2022-12-23 22:12:08 +0530

    about

--- express/server.js
+++ express/server.js
@@ -5,6 +5,9 @@ const port = 5000
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+app.get('/about',(req,res)=>{
+    res.send('hello about')
+})
 app.listen(port,()=>{
     console.log(`server is running on ${port}`);
 })
