52d3eb3703d043600c266574f3d966072f42939a
Sudarshan_N <109232158+ZoRoRooT@users.noreply.github.com>
2022-12-26 21:45:50 +0530

    hi

--- node/pathmodule.js
+++ node/pathmodule.js
@@ -1,5 +1,5 @@
 const path = require('path')
 const a = path.dirname('C:\\user\\Desktop\\app1');
 console.log(a);
-const a1 =path.extname(__filename)
+const a1 =path.extname (__filename)
 console.log(a1);
